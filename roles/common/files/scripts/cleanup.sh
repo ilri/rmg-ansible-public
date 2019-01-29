ed8348badb17a3b35bf374c0f1ecaf6557978e89
Alan Orth <alan.orth@gmail.com>
2019-01-29 10:47:04 +0200

    roles/common: Remove schedtool from cleanup.sh
    Some older hosts don't have it and that causes the cleanup script to
    fail, which causes disks to get full, etc.

--- roles/common/files/scripts/cleanup.sh
+++ roles/common/files/scripts/cleanup.sh
@@ -6,4 +6,4 @@
 readonly BACKUP_DIR="/home/backup"
 
 # Find files older than 2 weeks and delete them
-schedtool -B -n1 -e ionice -c2 -n7 find -L ${BACKUP_DIR} -type f ! -newermt "2 weeks ago" -delete
+find -L ${BACKUP_DIR} -type f ! -newermt "2 weeks ago" -delete
